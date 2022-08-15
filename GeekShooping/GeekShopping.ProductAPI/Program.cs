64e1d1f3180d7c3d201904dd0534824326e3046d
Wesley Silva <wesleysa95@gmail.com>
2022-08-15 15:37:59 -0300

    Implementando o IProductRepository

--- GeekShooping/GeekShopping.ProductAPI/Program.cs
+++ GeekShooping/GeekShopping.ProductAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,10 @@ builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connecti
 IMapper mapper = Mappingconfig.RegisterMapps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+#endregion
+
+#region DI
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 #endregion
 
 builder.Services.AddControllers();
