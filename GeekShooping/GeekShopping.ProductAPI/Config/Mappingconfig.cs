0bb17f8132e03081e63fc894c864c99115c1a956
Wesley Silva <wesleysa95@gmail.com>
2022-08-15 15:19:25 -0300

    Confurando o AutoMapper

---
+++ GeekShooping/GeekShopping.ProductAPI/Config/Mappingconfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+
+namespace GeekShopping.ProductAPI.Config
+{
+    public class Mappingconfig
+    {
+        public static MapperConfiguration RegisterMapps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
--- GeekShooping/GeekShopping.ProductAPI/Program.cs
+++ GeekShooping/GeekShopping.ProductAPI/Program.cs
@@ -1,21 +1,32 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Add services to the container.
 var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 
+#region DbContexts
 builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 29))));
+#endregion
+
+#region AutoMapper
+IMapper mapper = Mappingconfig.RegisterMapps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+#endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+#region Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -27,3 +38,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+#endregion
