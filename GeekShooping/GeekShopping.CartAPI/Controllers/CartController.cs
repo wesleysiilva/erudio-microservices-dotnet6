1a433e8d99d9759b4da4c1b6422a19eb9b24d39f
Wesley Silva <wesleysa95@gmail.com>
2022-10-01 09:34:30 -0300

    ajustes

--- GeekShooping/GeekShopping.CartAPI/Controllers/CartController.cs
+++ GeekShooping/GeekShopping.CartAPI/Controllers/CartController.cs
@@ -16,9 +16,9 @@ namespace GeekShopping.CartAPI.Controllers
         }
 
         [HttpGet("find-cart/{id}")]
-        public async Task<ActionResult<CartVO>> FindById(string userId)
+        public async Task<ActionResult<CartVO>> FindById(string id)
         {
-            var cart = await _repository.FindCartByUserId(userId);
+            var cart = await _repository.FindCartByUserId(id);
             if (cart == null) return NotFound();
             return Ok(cart);
         }
