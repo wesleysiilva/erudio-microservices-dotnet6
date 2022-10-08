c059708b0d6184148564f3ac082b0e64c29e86c7
Wesley Silva <wesleysa95@gmail.com>
2022-10-08 17:28:50 -0300

    implementando o endpoint de checkout e a chamada no front

--- GeekShooping/GeekShopping.Web/Services/CartService.cs
+++ GeekShooping/GeekShopping.Web/Services/CartService.cs
@@ -67,9 +67,14 @@ namespace GeekShopping.Web.Services
             throw new Exception("Something went wrong when calling the API");
         }
 
-        public async Task<CartViewModel> Checkout(CartHeaderViewModel cartHeader, string token)
+        public async Task<CartHeaderViewModel> Checkout(CartHeaderViewModel model, string token)
         {
-            throw new NotImplementedException();
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.PostAsJson($"{BasePath}/checkout", model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<CartHeaderViewModel>();
+
+            throw new Exception("Something went wrong when calling the API");
         }
 
         public async Task<bool> ClearCart(string userId, string token)
