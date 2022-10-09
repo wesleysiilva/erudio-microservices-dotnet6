92f76803033541a7f7312dd66941eba67e2de434
Wesley Silva <wesleysa95@gmail.com>
2022-10-09 20:13:58 -0300

    Implementando o comportamento da validação do cupom do front

--- GeekShooping/GeekShopping.Web/Services/CartService.cs
+++ GeekShooping/GeekShopping.Web/Services/CartService.cs
@@ -67,12 +67,14 @@ namespace GeekShopping.Web.Services
             throw new Exception("Something went wrong when calling the API");
         }
 
-        public async Task<CartHeaderViewModel> Checkout(CartHeaderViewModel model, string token)
+        public async Task<object> Checkout(CartHeaderViewModel model, string token)
         {
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await _client.PostAsJson($"{BasePath}/checkout", model);
             if (response.IsSuccessStatusCode)
                 return await response.ReadContentAs<CartHeaderViewModel>();
+            else if (response.StatusCode.ToString().Equals("PreconditionFailed"))
+                return "Coupon Price has changed, please confirm!";
 
             throw new Exception("Something went wrong when calling the API");
         }
