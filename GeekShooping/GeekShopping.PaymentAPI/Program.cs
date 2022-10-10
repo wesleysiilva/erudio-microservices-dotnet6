36f7704ea603267fc0f5c7bbbe893dd0a4a3ad6e
Wesley Silva <wesleysa95@gmail.com>
2022-10-10 20:14:34 -0300

    Implementando o processamento de pagamento assíncrono

--- GeekShooping/GeekShopping.PaymentAPI/Program.cs
+++ GeekShooping/GeekShopping.PaymentAPI/Program.cs
@@ -1,25 +1,96 @@
+using GeekShopping.PaymentAPI.RabbitMQConsumer;
+using GeekShopping.PaymentAPI.RabbitMQSender;
+using GeekShopping.PaymentProcessor;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Add services to the container.
+#region DbContexts
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+//builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 29))));
+#endregion
+
+#region DI
+//var dbBuilder = new DbContextOptionsBuilder<MySQLContext>();
+//dbBuilder.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 29)));
+builder.Services.AddSingleton<IProcessPayment, ProcessPayment>();
+builder.Services.AddHostedService<RabbitMQPaymentConsumer>();
+builder.Services.AddSingleton<IRabbitMQSender, RabbitMQSender>();
+#endregion
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+#region Authentication/Authorization
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateAudience = false,
+        };
+    }
+);
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+#endregion
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+#region Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Geek Shoping.PaymentAPI", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id ="Bearer"
+                },
+                Scheme = "oauth2",
+                Name="Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+#endregion
+
+builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+#endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+#region Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GeekShopping.PaymentAPI v1"));
 }
-
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+#endregion
---
+++ GeekShooping/GeekShopping.PaymentAPI/RabbitMQConsumer/RabbitMQPaymentConsumer.cs
@@ -0,0 +1,69 @@
+﻿using GeekShopping.PaymentAPI.Messages;
+using GeekShopping.PaymentAPI.RabbitMQSender;
+using GeekShopping.PaymentProcessor;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace GeekShopping.PaymentAPI.RabbitMQConsumer
+{
+    public class RabbitMQPaymentConsumer : BackgroundService
+    {
+        private IConnection _connection;
+        private IModel _channel;
+        private readonly IProcessPayment _processPayment;
+        private readonly IRabbitMQSender _rabbitMQSender;
+
+        public RabbitMQPaymentConsumer(IProcessPayment processPayment, IRabbitMQSender rabbitMQSender)
+        {
+            _processPayment = processPayment;
+            _rabbitMQSender = rabbitMQSender;
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "orderpaymentprocessqueue", false, false, false, arguments: null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (chanel, evt) =>
+            {
+                var content = Encoding.UTF8.GetString(evt.Body.ToArray());
+                PaymentMessage vo = JsonSerializer.Deserialize<PaymentMessage>(content);
+                ProcessPayment(vo).GetAwaiter().GetResult();
+                _channel.BasicAck(evt.DeliveryTag, false);
+            };
+            _channel.BasicConsume("orderpaymentprocessqueue", false, consumer);
+            return Task.CompletedTask;
+        }
+
+        private async Task ProcessPayment(PaymentMessage vo)
+        {
+            var result = _processPayment.PaymentProcessor();
+            UpdatePaymentResultMessage paymentResultMessage = new()
+            {
+                Status = result,
+                Email = vo.Email,
+                OrderId = vo.OrderId,
+            };
+
+            try
+            {
+                _rabbitMQSender.SendMessage(paymentResultMessage, "orderpaymentresultqueue");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
